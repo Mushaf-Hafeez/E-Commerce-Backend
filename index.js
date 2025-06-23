190fb045c1dfbc326dbd7c54a6d10f07e46c875e
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-06-23 18:51:07 +0500

    implement cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const expressFileUpload = require("express-fileupload");
+const cors = require("cors");
 require("dotenv").config();
 
 // importing routes
@@ -22,6 +23,13 @@ connectCloudinary();
 
 // middlewares
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(
   expressFileUpload({
