66a007c20e76521477570b964b67b87a9d9a9e6f
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-07-02 15:45:13 +0500

    implement stripe

--- Controllers/order.controller.js
+++ Controllers/order.controller.js
@@ -1,10 +1,37 @@
 const Order = require("../Models/orderSchema.model");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // place order controller function
 exports.placeOrder = async (req, res) => {
   try {
-    const user = req.user;
-    console.log(user);
+    const { cartlist, address } = req.body;
+
+    const line_items = [];
+    cartlist.forEach((item) => {
+      line_items.push({
+        price_data: {
+          currency: "pkr",
+          product_data: {
+            name: item.productId.name,
+          },
+          unit_amount: item.productId.price * 100,
+        },
+        quantity: item.quantity,
+      });
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items,
+      mode: "payment",
+      success_url: `${process.env.FRONTEND_URL}/payment/success`,
+      cancel_url: `${process.env.FRONTEND_URL}/payment/cancel`,
+    });
+
+    return res.json({
+      url: session.url,
+    });
   } catch (error) {
     console.log(
       "Error in the place order controller function: ",
