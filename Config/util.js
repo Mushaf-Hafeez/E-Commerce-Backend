43559e6cd2a7511abf2556222903fe158ee344b0
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2026-03-12 11:15:21 +0500

    change cookie config

--- Config/util.js
+++ Config/util.js
@@ -8,7 +8,7 @@ exports.createCookie = (token, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
